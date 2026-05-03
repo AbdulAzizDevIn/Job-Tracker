81f918692716ee3cb61ee97d1aa3ac426897a2ce
Abdul Aziz <azizskwwe@gmail.com>
2026-05-04 00:25:19 +0530

    create models using mongoose

---
+++ lib/init-user-board.ts
@@ -0,0 +1,49 @@
+import connectDB from "./db";
+import { Board, Column } from "./models";
+
+const DEFAULT_COLUMNS = [
+  {
+    name: "Wish List",
+    order: 0,
+  },
+  {
+    name: "Applied",
+    order: 1,
+  },
+  {
+    name: "Interviewing",
+    order: 2,
+  },
+  {
+    name: "Offer",
+    order: 3,
+  },
+  {
+    name: "Rejected",
+    order: 4,
+  },
+];
+
+export async function initializeUserBoard(userId: string) {
+  try {
+    await connectDB();
+
+    // check if board  already exists
+    const existingBoard = await Board.findOne({ userId, name: "Job Hunt" });
+
+    if (existingBoard) {
+      return existingBoard;
+    }
+
+    // create the board
+    const board = await Board.create({
+      name: "Job Hunt",
+      userId,
+      columns: [],
+    });
+
+    // create default columns
+  } catch (error) {
+    throw error;
+  }
+}
